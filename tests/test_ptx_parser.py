d9f16153242f2d6f249991b8655829331de7b402
Muzammil Mahmud <54553139+mmahmud97@users.noreply.github.com>
2025-01-12 06:23:09 +0000

    update tests

--- tests/test_ptx_parser.py
+++ tests/test_ptx_parser.py
@@ -1,5 +1,10 @@
+import sys
+import os
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
 import pytest
 from ptx_parser import PtxParser
+
 
 
 def test_parse_single_kernel():
