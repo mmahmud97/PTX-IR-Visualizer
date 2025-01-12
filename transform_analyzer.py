ac46692df3fe483700705f9122fc288a3599d058
Muzammil Mahmud <54553139+mmahmud97@users.noreply.github.com>
2025-01-12 06:32:40 +0000

    fixing bugs

--- transform_analyzer.py
+++ transform_analyzer.py
@@ -15,38 +15,38 @@ class TransformAnalyzer:
     """
 
     def compare_kernels(self, ptx_a: Dict[str, Any], ptx_b: Dict[str, Any]) -> Dict[str, Any]:
-    """
-    Compares kernel data from two PTX sets.
-    Returns:
-        dict: A report highlighting changes in instruction count, new/removed kernels, etc.
-    """
-    report = {
-        "new_kernels": [],
-        "removed_kernels": [],
-        "changed_kernels": {}
-    }
+        """
+        Compares kernel data from two PTX sets.
+        Returns:
+            dict: A report highlighting changes in instruction count, new/removed kernels, etc.
+        """
+        report = {
+            "new_kernels": [],
+            "removed_kernels": [],
+            "changed_kernels": {}
+        }
 
-    kernels_a = set(ptx_a.keys())
-    kernels_b = set(ptx_b.keys())
+        kernels_a = set(ptx_a.keys())
+        kernels_b = set(ptx_b.keys())
 
-    # Identify new or removed kernels
-    report["new_kernels"] = list(kernels_b - kernels_a)
-    report["removed_kernels"] = list(kernels_a - kernels_b)
+        # Identify new or removed kernels
+        report["new_kernels"] = list(kernels_b - kernels_a)
+        report["removed_kernels"] = list(kernels_a - kernels_b)
 
-    # Identify changed kernels
-    common_kernels = kernels_a.intersection(kernels_b)
-    for kernel_name in common_kernels:
-        instructions_a = ptx_a[kernel_name]["instructions"]
-        instructions_b = ptx_b[kernel_name]["instructions"]
+        # Identify changed kernels
+        common_kernels = kernels_a.intersection(kernels_b)
+        for kernel_name in common_kernels:
+            instructions_a = ptx_a[kernel_name]["instructions"]
+            instructions_b = ptx_b[kernel_name]["instructions"]
 
-        if instructions_a != instructions_b:
-            diff = self.generate_instruction_diff(instructions_a, instructions_b)
-            if diff:
-                report["changed_kernels"][kernel_name] = {
-                    "instruction_diff": diff
-                }
+            if instructions_a != instructions_b:
+                diff = self.generate_instruction_diff(instructions_a, instructions_b)
+                if diff:
+                    report["changed_kernels"][kernel_name] = {
+                        "instruction_diff": diff
+                    }
 
-    return report
+        return report
 
 
     def generate_instruction_diff(self, instructions_a, instructions_b):
